c54a246dd0bddbeb63ecbb474d64f849c9b99452
KsyKsy-dev <ksy.codedev@gmail.com>
2024-05-06 07:43:22 +0200

    add imgs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("h");
+function burgerOpen() {
+  let burgerMenu = document.querySelector(".burger__menu");
+  burgerMenu.style.display = "flex";
+}
+function burgerClose() {
+  let burgerMenu = document.querySelector(".burger__menu");
+  burgerMenu.style.display = "none";
+  let screenWidth = window.innerWidth;
+  if (screenWidth > 1024) {
+  }
+}
+window.addEventListener("resize", function () {
+  burgerClose();
+});
